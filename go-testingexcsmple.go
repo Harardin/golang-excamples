868f8dc44bd28f65036ca5af0295ddee21e58c43
Anton Polchenko <Kreaitor@yandex.ru>
2020-11-14 10:09:32 +0300

    Create go-testingexcsmple.go
    Example on how to make test functions in golang

---
+++ go-testingexcsmple.go
@@ -0,0 +1,15 @@
+/*
+  Test file should contain _test at the end of the name
+  Command to run test: go test -run . "etc options"
+*/
+package main_test
+
+import (
+  "testing"
+)
+
+func Test_Main() {
+  /*
+    Some code to test
+  */
+}
